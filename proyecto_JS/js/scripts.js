ff6fafbee7bdd7bd7a3dc0c8dc67ac21853d5896
orionpgaete <pedro.gaete@pgcomputer.cl>
2021-10-27 22:09:54 -0300

    clase 1- JS -- 27.10

---
+++ proyecto_JS/js/scripts.js
@@ -0,0 +1,25 @@
+
+// alert("Hola Mundo !!!"); ejemplo de ALERTA
+
+
+//Variables
+
+var producto = "Mouse Gamer";   // iniciando una variable y asignando valor
+
+var disponible; //iniciado sin valor
+
+producto = true;
+
+disponible = true;
+
+var producto1 = "PC Gamer",
+    disponible1 = true,
+    categoria = "Computador";
+
+//estilos de varianbles
+var nombre_producto = "Monitor HD";
+var nombreproducto = "Monitor HD";
+var NombreProducto = 'Monitor HD - Pascal Case';
+
+
+console.log(NombreProducto);
