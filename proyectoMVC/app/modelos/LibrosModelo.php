869ecc19159a5f2d5dc1469c42893f21c0463666
orionpgaete <pedro.gaete@pgcomputer.cl>
2021-12-23 19:36:24 -0300

    clase 23-12

--- proyectoMVC/app/modelos/LibrosModelo.php
+++ proyectoMVC/app/modelos/LibrosModelo.php
@@ -1,20 +1,63 @@
 <?php
-
+/**
+ * Libros Modelo
+ */
 class LibrosModelo{
+  private $db;
+  
+  function __construct()
+  {
+    $this->db = new MySQLdb();
+  }
 
-    private $db;
+  public function getLibros(){
+    $data = $this->db->querySelect("SELECT * FROM libros");
+    return $data;
+  }
 
-    function __construct()
-    {
-        $this->db = new MySQLdb();
+  public function getLibro($id){
+    $data = $this->db->querySelect("SELECT * FROM libros WHERE id=".$id);
+    return $data;
+  }
+
+  public function insertarLibros($titulo, $autor, $editorial, $isdn){
+    $sql = "INSERT INTO libros VALUES(0,";
+    $sql.= "'".$titulo."', ";
+    $sql.= "'".$autor."', ";
+    $sql.= "'".$editorial."', ";
+    $sql.= "'".$isdn."')";
+    if ($this->db->queryNoSelect($sql)) {
+      header("location:".RUTA_APP."libros");
+      exit();
+    } else {
+      exit("Error al insertar los datos del libro");
     }
+  }
 
-    public function getLibros()
-    {
-      $data = $this->db->querySelect("SELECT * FROM libros");
-      return $data;
+  public function modificarLibros($id, $titulo, $autor, $editorial){
+    $sql = "UPDATE libros SET ";
+    $sql.= "titulo='".$titulo."', ";
+    $sql.= "autor='".$autor."', ";
+    $sql.= "editorial='".$editorial."' ";
+    $sql.= "WHERE id=".$id;
+    print $sql;
+    if ($this->db->queryNoSelect($sql)) {
+      header("location:".RUTA_APP."libros");
+      exit();
+    } else {
+      exit("Error al modificar los datos del libro");
     }
+  }
 
+  public function borrarLibro($id){
+    $sql = "DELETE FROM libros ";
+    $sql.= "WHERE id=".$id;
+    if ($this->db->queryNoSelect($sql)) {
+      header("location:".RUTA_APP."libros");
+      exit();
+    } else {
+      exit("Error al borrar el libro");
+    }
+  }
 }
-
 ?>
